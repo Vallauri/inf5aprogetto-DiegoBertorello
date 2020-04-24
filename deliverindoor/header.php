a664256e14161028734b1f9d4108ede1151441bd
DiegoBertorello <62566751+DiegoBertorello@users.noreply.github.com>
2020-04-24 19:54:21 +0200

    Home done

--- deliverindoor/header.php
+++ deliverindoor/header.php
@@ -2,30 +2,21 @@
     echo '
         <nav class="navbar navbar-expand-lg fixed-top">
             <div class="container">
-                <a class="navbar-brand" href="#">Deliverindoor</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li>
-                            <div class="buscar-caja"> 
-                                <input type="text" name="" class="buscar-txt" placeholder="Search ....."/> 
-                                <a class="buscar-btn"> <i class="fa fa-search"></i> </a> 
-                            </div>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-                                <i class="fa fa-user"></i>
-                            </a>
-                            <div class="dropdown-menu">
-                                <a class="dropdown-item" href="#">Area Personale</a>
-                                <a class="dropdown-item" href="#">Segnalazioni</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#">Log out</a>
-                            </div>
-                        </li>
-                    </ul>
+                <a class="navbar-brand" href="#">
+                    <img src="img/logo.png">
+                </a>
+                <div  id="navbarResponsive">
+                    <div class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                            <i class="fa fa-user"></i>
+                        </a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Area Personale</a>
+                            <a class="dropdown-item" href="#">Segnalazioni</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Log out <i class="fas fa-sign-out-alt"></i> </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </nav>
--- deliverindoor/index.php
+++ deliverindoor/index.php
@@ -31,58 +31,80 @@
             require "header.php";
         ?>
 
-        <!--Carousel--> 
-        <div class="container d-flex justify-content-center">
-            <div  class="col-lg-10">
-                <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner" role="listbox">
-                    <div class="carousel-item active">
-                    <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide">
-                    </div>
-                    <div class="carousel-item">
-                    <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
-                    </div>
-                    <div class="carousel-item">
-                    <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
-                    </div>
+        <!--Carousel-->
+        <section class="col-lg-11 caro">
+            <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                <img class="d-block w-100" src="http://placehold.it/900x350" alt="First slide">
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="http://placehold.it/900x350" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="http://placehold.it/900x350" alt="Third slide">
                 </div>
             </div>
-        </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+            </div>
+        </section>
+        <section class="search-sec col-lg-11">
+            <div class="container">
+                <form action="#" method="post" novalidate="novalidate">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="row">
+                                <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                                    <input type="text" class="form-control search-slt" placeholder="Inserisci nome dell'attivit&agrave...">
+                                </div>
+                                <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                                    <input type="text" class="form-control search-slt" placeholder="Inserisci la citt&agrave...">
+                                </div>
+                                <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                                    <select class="form-control search-slt" id="exampleFormControlSelect1">
+                                        <option>Pizzerie</option>
+                                        <option>Ristoranti</option>
+                                        <option>Panetterie</option>
+                                        <option>Macellerie</option>
+                                    </select>
+                                </div>
+                                <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                                    <button type="button" class="btn btn-danger wrn-btn">Cerca</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
 
         <!-- Icons Grid -->
-        <section class="features-icons bg-light text-center">
+        <section class="features-icons text-center" style="margin-bottom: 30px;">
             <div class="container">
             <div class="row">
-                <div class="col-lg-3">
+                <div class="col-lg-3 col-sm-6 col-6">
                     <div class="categ">
                         <img src="img/pizza.svg" title="Pizzerie">
                     </div>
                 </div>
-                <div class="col-lg-3">
+                <div class="col-lg-3 col-sm-6 col-6">
                     <div class="categ">
                         <img src="img/restaurant.svg" title="Ristoranti">
                     </div>
                 </div>
-                <div class="col-lg-3">
+                <div class="col-lg-3 col-sm-6 col-6">
                     <div class="categ">
                         <img src="img/bread.svg" title="Panetterie">
                     </div>
                 </div>
-                <div class="col-lg-3">
+                <div class="col-lg-3 col-sm-6 col-6">
                     <div class="categ">
                         <img src="img/meat.svg" title="Macellerie"> 
                     </div> 
@@ -90,6 +112,66 @@
             </div>
             </div>
         </section>
+
+        <!-- Card -->
+        <!--<div class="container d-flex justify-content-center" style="margin-top: 5px;">
+            <div class="col-lg-2 bg-light" style="border-radius:10px 0 0 10px;">
+                <img src="http://placehold.it/150x150">
+            </div>
+            <div class="col-lg-5 bg-light" style="height:150px; border-radius:0 10px 10px 0;">
+                <h3>Prova prova prova</h3>
+                <p>Prova descrizione qwertyuiopojhgfdsascvbnmmjhgfdwerty</p>
+            </div>
+        </div>-->
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card h-300">
+                        <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                        <div class="card-body">
+                            <h4 class="card-title">
+                            <a href="#">Prova negozio</a>
+                            </h4>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card h-300">
+                        <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                        <div class="card-body">
+                            <h4 class="card-title">
+                            <a href="#">Prova negozio</a>
+                            </h4>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 mb-4">
+                    <div class="card h-300">
+                        <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                        <div class="card-body">
+                            <h4 class="card-title">
+                            <a href="#">Prova negozio</a>
+                            </h4>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur!</p>
+                        </div>
+                        <div class="card-footer">
+                            <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
 
         <?php 
             require "footer.php";
